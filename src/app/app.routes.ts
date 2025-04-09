94b8e6f4daba5b3a474f179a0731bda8f37d29cb
amiine <amineouanda@gmail.com>
2025-04-09 17:28:54 +0100

    la page du projets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {AccueilComponent} from './accueil/accueil.component';
 import {AboutComponent} from './about/about.component';
+import {ProjetsComponent} from './projets/projets.component';
 
 export const routes: Routes = [
   {path :"accueil" , component : AccueilComponent },
-  {path : "about" , component : AboutComponent}
+  {path : "about" , component : AboutComponent},
+  {path : "projets" , component : ProjetsComponent}
 ];
--- src/app/projets/projets.component.ts
+++ src/app/projets/projets.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-projets',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './projets.component.html',
   styleUrl: './projets.component.css'
 })
-export class ProjetsComponent {
+export class ProjetsComponent implements OnInit{
+
+  public projets =[
+    {"nom" : "edumanage" , "image" : "/edu.jpg " ,"repostry" : "linkrepo" , "languag" : "java"},
+    {"nom" : "constructionXpert" , "image" : "/construction.jpg " ,"repostry" : "https://github.com/Ouanda-amine/ConstructionXpert" ,"languag" : "java"},
+    {"nom" : "meteo" , "image" : "/mte.jpg " ,"repostry" : "https://github.com/Ouanda-amine/Meteo" , "languag" : "js"},
+    {"nom" : "Sportflow" , "image" : "/sport.jpg " ,"repostry" : "https://github.com/Ouanda-amine/SportFlow", "languag" : "java"},
+    {"nom" : "movies" , "image" : "movie.jpg " ,"repostry" : "https://github.com/Ouanda-amine/Movies" , "languag" : "html"},
+    {"nom" : "SmartRecruit" , "image" : "smart.jpg " ,"repostry" : "https://github.com/Ouanda-amine/SmartRecruit" , "languag" : "java"}
+
+  ];
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 
 }
