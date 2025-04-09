19456e4e97e62845264dfb9179f47b4e11bf9233
amiine <amineouanda@gmail.com>
2025-04-09 12:38:27 +0100

    la page about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {AccueilComponent} from './accueil/accueil.component';
+import {AboutComponent} from './about/about.component';
 
 export const routes: Routes = [
-  {path :"accueil" , component : AccueilComponent }
+  {path :"accueil" , component : AccueilComponent },
+  {path : "about" , component : AboutComponent}
 ];
